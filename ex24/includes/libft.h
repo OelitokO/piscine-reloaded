971010ab7dc78760c2ceaa43ab57c4ff405b6699
Ömer Orhan <omerelitok.05@gmail.com>
2025-05-17 22:55:27 +0300

    ex24 eklendi

---
+++ ex24/includes/libft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: OelitokO <omerelitok.05@gmail.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/17 21:01:06 by OelitokO          #+#    #+#             */
+/*   Updated: 2025/05/17 21:19:05 by OelitokO         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+void	ft_swap(int *a, int *b);
+
+#endif
